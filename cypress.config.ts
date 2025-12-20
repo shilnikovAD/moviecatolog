0ced46ce355d80b010d87ed8f597443dd8cf74ba
arturshilnikov <shilnikov.ad@phystech.edu>
2025-12-20 16:24:40 +0300

    demo version 2

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5180',
+    baseUrl: 'http://localhost:5174',
   },
   component: {
     devServer: {
       framework: 'react',
       bundler: 'vite',
     },
+    supportFile: 'cypress/support/component.ts',
   },
 });
